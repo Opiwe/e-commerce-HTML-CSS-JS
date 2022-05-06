b38cd1a45cd6ae1fc627e847e8cec481f324bbe4
Nkqubela Jojo <jnkqubela@gmail.com>
2022-05-06 14:06:10 +0200

    main.js added onclick cart

--- js/main.js
+++ js/main.js
@@ -2,4 +2,11 @@
 let cartIcon = document.querySelector('#cart-icon');
 let cart = document.querySelector('.cart');
 let closeCart = document.querySelector('#close-cart');
-
+// Open Cart
+cartIcon.onclick = () => {
+    cart.classList.add("active");
+}
+// Close Cart
+closeCart.onclick = () => {
+    cart.classList.remove("active");
+}
