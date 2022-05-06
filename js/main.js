c72d48072981fbc5e59ef0fc8aa9475abf1ca9b1
Nkqubela Jojo <jnkqubela@gmail.com>
2022-05-06 16:08:15 +0200

    removed cart-box in index.html

--- js/main.js
+++ js/main.js
@@ -78,7 +78,15 @@ function  addProductToCart(title, price, productImg){
         alert("You have already add this item to cart");
     }
 }
-
+var cartBoxContent = `
+                        <img src="./img/product2.jpeg" alt="" class="cart-img">
+                        <div class="detail-box">
+                            <div class="cart-product-title">Earbuds</div>
+                            <div class="cart-price">R1571,48</div>
+                            <input type="number" value="1" class="cart-quanity">
+                        </div>
+                        <!-- Remove Cart -->
+                        <i class='bx bxs-trash-alt cart-remove'></i>`;
 
 // Update Total
 function updateTotal(){ 
