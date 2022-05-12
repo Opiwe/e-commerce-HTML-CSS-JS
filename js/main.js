5bfad0bac3bf76372ea280e5a0d3d233a404955a
Nkqubela Jojo <jnkqubela@gmail.com>
2022-05-12 13:57:26 +0200

    added ; and changed let to var

--- js/main.js
+++ js/main.js
@@ -24,11 +24,11 @@ else{
 function ready(){
     // Remove Items From Cart
     var removeCartButtons = document.getElementsByClassName('cart-remove');
-    console.log(removeCartButtons)
+    console.log(removeCartButtons);
 
-    for (let i = 0; i < removeCartButtons.length; i++) {
+    for (var i = 0; i < removeCartButtons.length; i++) {
         var button = removeCartButtons[i];
-        button.addEventListener('click', removeCartItem)
+        button.addEventListener('click', removeCartItem);
     }
     // Quantity Changes
     var quanityInputs = document.getElementsByClassName('cart-quanity');
@@ -104,12 +104,12 @@ function updateTotal(){
     var cartContent = document.getElementsByClassName('cart-content')[0];
     var cartBoxes = cartContent.getElementsByClassName('cart-box');
     var total = 0;
-    for (let i = 0; i < cartBoxes.length; i++){
+    for (var i = 0; i < cartBoxes.length; i++){
         var cartBox = cartBoxes[i];
         var priceElement = cartBox.getElementsByClassName('cart-price')[0];
         var quantityElement = cartBox.getElementsByClassName('cart-quanity')[0];
         var price = parseFloat(priceElement.innerText.replace("R", ""));
-        var quanity = quantityElement.value
+        var quanity = quantityElement.value;
         total = total + price * quanity;
         //If price Contain some cents Value 
         total = Math.round(total * 100) / 100; 
